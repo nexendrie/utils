74cede8f6e8cb9bfdb83b23584e517cc2a6e86e9
Jakub Konečný <jakub.konecny2@centrum.cz>
2026-01-24 18:01:04 +0100

    do not use Nette DI container in tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,7 +2,4 @@
 declare(strict_types=1);
 
 require __DIR__ . "/../vendor/autoload.php";
-Testbench\Bootstrap::setup(__DIR__ . '/_temp', static function (\Nette\Bootstrap\Configurator $configurator): void {
-    $configurator->addStaticParameters(["appDir" => __DIR__,]);
-    $configurator->addConfig(__DIR__ . "/tests.neon");
-});
+Testbench\Bootstrap::setup(__DIR__ . '/_temp');
